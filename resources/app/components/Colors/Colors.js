11e1d976c8dda1feee3441d135f565f176b93ceb
Jan <jan@glue.be>
2019-03-01 09:29:59 +0100

    updated text

--- resources/app/components/Colors/Colors.js
+++ resources/app/components/Colors/Colors.js
@@ -10,7 +10,7 @@ class Colors extends React.Component{
         <div className="mb-8">
           <h2 className="mb-6">Colors</h2>
           <div className='richtext mb-8'>
-            <p>Colors are taken from the Layer Styles. When adding additional colors, try to use variable names for the colors</p>
+            <p>Colors are taken from the Layer Styles. When adding additional colors, try to use variable names for the names and hex for the colors.</p>
           </div>
           <label className="mb-4">Active colors</label>
           {Object.keys(this.props.colors).map(key =>
