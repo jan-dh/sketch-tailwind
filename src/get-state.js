d008bf34a1fe5b0094375d0f7d972d92aef12924
Jan <jan@glue.be>
2019-11-16 18:02:00 +0100

    update deprecated api

--- src/get-state.js
+++ src/get-state.js
@@ -1,13 +1,12 @@
-import sketch from 'sketch';
-import {getLastPart} from './helpers';
-import fs from '@skpm/fs';
-import util from 'util';
-
+import sketch from "sketch";
+import { getLastPart } from "./helpers";
+import fs from "@skpm/fs";
+import util from "util";
 
 export default function() {
   const doc = sketch.fromNative(context.document);
-  const layers = doc.getSharedLayerStyles();
-  const textLayers = doc.getSharedTextStyles();
+  const layers = doc.sharedLayerStyles();
+  const textLayers = doc.sharedTextStyles();
   const state = {};
 
   // Get colors
@@ -27,31 +26,41 @@ export default function() {
   }
   // Get font family
   function getFontFamilies(layers) {
-    const names = layers.map($layer => $layer.style.sketchObject.textStyle().attributes().NSFont.familyName());
-    const fontFamilies = names.map((name) => (
-        {
-          name: String(name),
-          value: String(name).replace(/\s+/g, '-').toLowerCase()
-        }
-      )
+    const names = layers.map($layer =>
+      $layer.style.sketchObject
+        .textStyle()
+        .attributes()
+        .NSFont.familyName()
     );
+    const fontFamilies = names.map(name => ({
+      name: String(name),
+      value: String(name)
+        .replace(/\s+/g, "-")
+        .toLowerCase()
+    }));
     // Remove duplicates
-    const cleanFamilies = fontFamilies.map(e => e['name'])
+    const cleanFamilies = fontFamilies
+      .map(e => e["name"])
       .map((e, i, final) => final.indexOf(e) === i && i)
-      .filter(e => fontFamilies[e]).map(e => fontFamilies[e]);
+      .filter(e => fontFamilies[e])
+      .map(e => fontFamilies[e]);
     return cleanFamilies;
   }
   // Get Font sizes
   function getFontSizes(layers) {
-    let fontSizes = {}
-    const sizes = layers.map($layer => $layer.style.sketchObject.textStyle().attributes().NSFont.pointSize());
-    Object.values(sizes).forEach(($size) => {
+    let fontSizes = {};
+    const sizes = layers.map($layer =>
+      $layer.style.sketchObject
+        .textStyle()
+        .attributes()
+        .NSFont.pointSize()
+    );
+    Object.values(sizes).forEach($size => {
       // Set empty text for each size
-      fontSizes[$size] = '';
+      fontSizes[$size] = "";
     });
     return fontSizes;
   }
-
 
   // Set state
   state.colors = getColors(layers);
